96045001a6abd2f0f6bee25ded5c3df5d3cde3fa
Ayush Pratap <ayushpratap55@gmail.com>
2023-04-22 20:04:46 +0530

    add instance

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from flask import Flask, render_template
 from forms import AdminRegistrationForm
 from forms import AdminLoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SCRET_KET']='secret_key_33'
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///site.db"
+
+db=SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(60), nullable=False)
+    password=db.Column(db.String(32), nullable=False)
+
+    def __repr__(self):
+        return f"User(email: {self.email})"
+
+class Book(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(60), nullable=False)
+    author=db.Column(db.String(60), nullable=False)
+    isbn=db.Column(db.Integer, unique=True, nullable=False)
+    genre=db.Column(db.String(60), nullable=False)
+    shop_link=db.Column(db.String(100), nullable=False)
+    rating=db.Column(db.Float, nullable=False)
+    cover=db.Column(db.String(120), nullable=True, default="default.jpg")
+    tiny_sum=db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Book(title: {self.title}, isbn: {self.isbn})"
 
 msg='Ayush Pratap'
 
@@ -24,7 +54,12 @@ def register():
     #rendering(returning) HTML file
     form = AdminRegistrationForm()
     if form.validate_on_submit():
-        print(form.data)
+        _email=form.data['email']
+        _password=form.data['password']
+        user=User(email=_email, password=_password)
+        db.session.add(user)
+        db.session.commit()
+        print("User added")
     return render_template("register.html", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
@@ -34,6 +69,14 @@ def login():
     if form.validate_on_submit():
         print(form.data)
     return render_template("login.html", form=form)
+
+@app.route("/addbook", methods=["GET", "POST"])
+def addbook():
+    #rendering(returning) HTML file
+    form = AdminRegistrationForm()
+    if form.validate_on_submit():
+        print(form.data)
+    return render_template("addbook.html", form=form)
 
 
 @app.route("/about")
